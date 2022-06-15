c9e1e04d03cb1560084e9263b0525a7fded505c3
Anthony Hong <tony.hongq@gmail.com>
2022-06-15 12:48:35 -0700

    add contact links

--- src/components/subcomponents/SocialMedia.jsx
+++ src/components/subcomponents/SocialMedia.jsx
@@ -1,23 +1,34 @@
 import React from 'react'
 import {AiOutlineMail} from 'react-icons/ai';
 import {FaLinkedinIn, FaGithub} from 'react-icons/fa';
-import {BsFillPersonLinesFill} from 'react-icons/bs';
+import {BsWhatsapp} from 'react-icons/bs';
+
 
 function SocialMedia({custom}) {
 	return (
 		<>
+			<a href="https://www.linkedin.com/in/anthony-d-hong/" target='_blank' rel='noreferrer'>
+				<div className={`social-icon mr-4 ${custom} `}>
+					<FaLinkedinIn />
+				</div>
+			</a>
+			<a href="https://github.com/hong710" target="_blank" rel='noreferrer' >
+				<div className={`social-icon mr-4 ${custom} `}>
+					<FaGithub />
+				</div>
+			</a>
+
+			<a href = "mailto: anthonyhong.d@gmail.com">
+				<div className={`social-icon mr-4 ${custom} `}>
+					<AiOutlineMail />
+				</div>
+			</a>
+
+			<a href = "https://api.whatsapp.com/send?phone=19177692518" target="_blank" rel='noreferrer'>
 			<div className={`social-icon mr-4 ${custom} `}>
-				<FaLinkedinIn />
+				<BsWhatsapp />
 			</div>
-			<div className={`social-icon mr-4 ${custom} `}>
-				<FaGithub />
-			</div>
-			<div className={`social-icon mr-4 ${custom} `}>
-				<AiOutlineMail />
-			</div>
-			<div className={`social-icon mr-4 ${custom} `}>
-				<BsFillPersonLinesFill />
-			</div>
+			</a>	
 		</>
 	)
 }
